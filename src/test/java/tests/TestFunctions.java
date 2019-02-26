6ba6533bb070a0cd2941277ceaff8e57a4117a50
Sergei <sleepytomcat@gmail.com>
2019-02-25 23:30:32 -0800

    Function lifting

--- src/test/java/tests/TestFunctions.java
+++ src/test/java/tests/TestFunctions.java
@@ -1,6 +1,8 @@
 package tests;
 
 import io.vavr.*;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -26,7 +28,7 @@ public class TestFunctions {
 		Function2<Integer, Integer, Integer> sum = (x, y) -> x + y;
 		Function1<Integer, Integer> multiplyBy3 = x -> x * 3;
 		Function2<Integer, Integer, Integer> sumAndMultiplyBy2 = sum.andThen(multiplyBy3);
-		Assert.assertEquals(sumAndMultiplyBy2.apply(1, 2), Integer.valueOf((1+2)*3));
+		Assert.assertEquals(sumAndMultiplyBy2.apply(1, 2), Integer.valueOf((1 + 2) * 3));
 	}
 
 	@Test
@@ -34,20 +36,20 @@ public class TestFunctions {
 		// reversed order of args
 		Function2<Float, Float, Float> div = (x, y) -> x / y;
 		Function2<Float, Float, Float> reversedDiv = div.reversed();
-		Assert.assertEquals(div.apply(2f, 3f), Float.valueOf(2f/3f));
-		Assert.assertEquals(reversedDiv.apply(2f, 3f), Float.valueOf(3f/2f));
+		Assert.assertEquals(div.apply(2f, 3f), Float.valueOf(2f / 3f));
+		Assert.assertEquals(reversedDiv.apply(2f, 3f), Float.valueOf(3f / 2f));
 
 		// curried function
 		Function1<Float, Function1<Float, Float>> curriedDiv = div.curried();
-		Assert.assertEquals(curriedDiv.apply(3f).apply(4f), Float.valueOf(3f/4f));
+		Assert.assertEquals(curriedDiv.apply(3f).apply(4f), Float.valueOf(3f / 4f));
 
 		// tupled
 		Function1<Tuple2<Float, Float>, Float> tupledDiv = div.tupled();
-		Tuple2<Float, Float> args =  Tuple.of(2f, 3f);
-		Assert.assertEquals(tupledDiv.apply(args), Float.valueOf(2f/3f));
+		Tuple2<Float, Float> args = Tuple.of(2f, 3f);
+		Assert.assertEquals(tupledDiv.apply(args), Float.valueOf(2f / 3f));
 
 		// partially applied
-		Function3<Integer, Integer, Integer, Integer> sum = (x, y, z)-> x + y + z;
+		Function3<Integer, Integer, Integer, Integer> sum = (x, y, z) -> x + y + z;
 		Function2<Integer, Integer, Integer> f = sum.apply(10); // partially applied, first argument bound
 		Assert.assertEquals(f.apply(3, 4), Integer.valueOf(10 + 3 + 4));
 
@@ -71,5 +73,23 @@ public class TestFunctions {
 
 	private static String helperMethod(Integer number) {
 		return String.valueOf(number);
+	}
+
+	@Test
+	public void functionLifting() {
+		Function2<Integer, Integer, Integer> div = (x, y) -> x / y;
+
+		// div is partial function (not defined for some input values)
+		Assert.expectThrows(ArithmeticException.class, () -> div.apply(1, 0));
+
+		// lifted to Option<Integer>
+		Function2<Integer, Integer, Option<Integer>> liftedToOption = Function2.lift(div);
+		Option<Integer> maybeNumber = liftedToOption.apply(1, 0);
+		Assert.assertTrue(maybeNumber.isEmpty());
+
+		// lifted to Try<Integer>
+		Function2<Integer, Integer, Try<Integer>> liftedToTry = Function2.liftTry(div);
+		Try<Integer> tryNumber = liftedToTry.apply(1, 0);
+		Assert.assertTrue(tryNumber.isFailure());
 	}
 }
