1e43002078bd651b51b606df69089cf53764023f
Sergei <sleepytomcat@gmail.com>
2020-05-10 10:42:51 -0700

    Tuple use cases added

--- src/test/java/tests/TestTuples.java
+++ src/test/java/tests/TestTuples.java
@@ -1,15 +1,19 @@
 package tests;
 
 import io.vavr.Tuple;
+import io.vavr.Tuple1;
 import io.vavr.Tuple2;
 import io.vavr.Tuple8;
+import io.vavr.collection.Seq;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestTuples {
 	@Test
-	public void of() {
+	void of() {
 		// Tuple2
 		Tuple2<String, String> tuple = Tuple.of("hello", "world");
 
@@ -32,13 +36,18 @@ public class TestTuples {
 	}
 
 	@Test
-	public void map() {
+	void map() {
 		Tuple2<String, String> tuple = Tuple.of("hello", "world");
 
-		// map
+		// map, with BiFunction<>
 		Tuple2<Integer, Integer> lengthTuple = tuple.map((a,b) -> Tuple.of(a.length(), b.length()));
 		assertEquals(lengthTuple._1.intValue(), "hello".length());
 		assertEquals(lengthTuple._2.intValue(), "world".length());
+
+		// map, with two Function<>
+		Tuple2<Integer, Integer> anotherLengthTuple = tuple.map(String::length, String::length);
+		assertEquals(anotherLengthTuple._1.intValue(), "hello".length());
+		assertEquals(anotherLengthTuple._2.intValue(), "world".length());
 
 		// map1
 		Tuple2<Integer, String> firstValueMappedTuple = tuple.map1(String::length);
@@ -52,7 +61,7 @@ public class TestTuples {
 	}
 
 	@Test
-	public void apply() {
+	void apply() {
 		Tuple2<String, String> tuple = Tuple.of("hello", "world");
 
 		String evaluated = tuple.apply((a, b) -> "first value " + a + " second value " + b);
@@ -60,7 +69,7 @@ public class TestTuples {
 	}
 	
 	@Test
-	public void update() {
+	void update() {
 		Tuple2<String, String> tuple = Tuple.of("hello", "world");
 
 		// update1
@@ -73,4 +82,41 @@ public class TestTuples {
 		assertEquals(everyoneTuple._1, "hello");
 		assertEquals(everyoneTuple._2, "everyone");
    }
+
+	@Test
+	void concat() {
+		Tuple1<String> first = Tuple.of("answer");
+		Tuple1<Integer> second = Tuple.of(42);
+
+		Tuple2 both = first.concat(second);
+		assertEquals(both._1, "answer");
+		assertEquals(both._2, 42);
+	}
+
+	@Test
+	void swap() {
+		Tuple2<String, Integer> tuple = Tuple.of("answer", 42);
+
+		assertEquals(tuple._1, tuple.swap()._2);
+		assertEquals(tuple._2, tuple.swap()._1);
+	}
+
+	@Test
+	void sequence() {
+		Iterable<Tuple2<String, Integer>> indexedDays = Arrays.asList(
+				Tuple.of("Monday", 1),
+				Tuple.of("Tuesday", 2),
+				Tuple.of("Wednesday", 3),
+				Tuple.of("Thursday", 4),
+				Tuple.of("Friday", 5),
+				Tuple.of("Saturday", 6),
+				Tuple.of("Sunday", 7)
+		);
+
+		Tuple2<Seq<String>, Seq<Integer>> days = Tuple.sequence2(indexedDays);
+		Seq<String> dayNames = days._1;
+		Seq<Integer> dayIndexes = days._2;
+		assertEquals(7, dayNames.size());
+		assertEquals(7, dayIndexes.size());
+	}
 }
