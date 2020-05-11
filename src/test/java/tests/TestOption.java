d009f7398669f7b3406b0ae8e8faf904275aaac8
Sergei <sleepytomcat@gmail.com>
2020-05-10 23:39:56 -0700

    More Option use cases

--- src/test/java/tests/TestOption.java
+++ src/test/java/tests/TestOption.java
@@ -3,85 +3,69 @@ package tests;
 import io.vavr.control.Option;
 import org.junit.jupiter.api.Test;
 
-import java.util.NoSuchElementException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestOption {
+class TestOption {
 	@Test
-	public void optionCreation() {
-		Option<Integer> maybe1 = Option.of(1);
-		Option<Integer> maybe2 = Option.some(2);
-		Option<String> maybeString = Option.of("some text");
-		assertEquals(maybe1.get(), Integer.valueOf(1));
-		assertEquals(maybe2.get(), Integer.valueOf(2));
-		assertEquals(maybeString.get(), "some text");
+	void creation() {
+		Option<String> maybe1 = Option.some("text");
+		assertEquals(maybe1.get(), "text");
 
-		Option<String> maybeNull = Option.of(null); // valid in vavr; yields Option.none()
-		Option<String> maybeOtherNull = Option.none(); // same as Option.of(null)
-		assertTrue(maybeNull.isEmpty());
-		assertTrue(maybeOtherNull.isEmpty());
-		// this will throw NoSuchElementException as maybeNull is Option.none()
-		assertThrows(NoSuchElementException.class, () -> maybeNull.get());
+		Option<String> maybe2 = Option.some(null); // Option.some(null) is valid in Vavr
+		assertFalse(maybe2.isEmpty());
+		assertEquals(maybe2.get(), null);
+
+		Option<String> maybe3 = Option.none();
+		assertTrue(maybe3.isEmpty());
+
+		Option<String> maybe4 = Option.of("text"); // returns Option.some("text")
+		assertEquals(maybe4.get(), "text");
+
+		Option<String> maybe5 = Option.of(null); // returns Option.none(); NPE-safe
+		assertTrue(maybe5.isEmpty());
 	}
 
 	@Test
-	public void optionNone() {
-		assertTrue(Option.none().isEmpty()); // filter yields Option.none()
-		assertEquals(Option.none().getOrElse(3), Integer.valueOf(3));
-		assertEquals(Option.none().getOrElse(() -> 3), Integer.valueOf(3));
+	void filter() {
+		assertEquals(Option.of(1).filter(x -> x > 0).get(), 1); // filter yields Option.some(1)
 		assertTrue(Option.of(1).filter(x -> x > 10).isEmpty()); // filter yields Option.none()
 	}
 
 	@Test
-	public void optionTransformations() {
-		Integer num = Option.of("hello")
-				.map(s -> s.length())
-				.map(l -> l * 2)
-				.getOrElse(-1);
-		assertEquals(num, Integer.valueOf("hello".length() * 2));
-
-		Integer otherNum = Option.<String>of(null) // same as Option.none()
-				.map(s -> s.length())
-				.map(l -> l * 2)
-				.getOrElse(-1); // yields -1 because value is Option.none()
-		assertEquals(otherNum, Integer.valueOf(-1));
-
-		String greeting = Option.of("hello")
-				.flatMap(s -> Option.of(s + "!"))
-				.getOrElse("");
-		assertEquals(greeting, "hello!");
-	}
-
-	@Test
-	public void optionComparison() {
+	void equals() {
 		Option<String> maybeText1 = Option.of("he" + "llo");
 		Option<String> maybeText2 = Option.of("hel" + "lo"); // to ensure the strings are not interned
 		assertTrue(maybeText1.equals(maybeText2));
 	}
 
 	@Test
-	public void someOfNullControversy() {
-		assertThrows(NullPointerException.class, () -> helperMethodWrong());
-	}
+	void flatMap() {
+		Option<Integer> wordLength = Option.of("text").flatMap(s -> Option.of(s.length()));
 
-	static Integer helperMethodWrong() {
-		return Option.of("hello")
-				.map(str -> (String) null) // results in Option.some(null)
-				.map(str -> str.length()) // NullPointerException, as str is null
-				.getOrElse(-1);
+		assertFalse(wordLength.isEmpty());
+		assertEquals(wordLength.get(), "text".length());
 	}
 
 	@Test
-	public void handlingNullRightWay() {
-		assertEquals(helperMethodRight(), Integer.valueOf(-1));
+	void map() {
+		Option<Integer> wordLength = Option.of("text").map(String::length);
+
+		assertFalse(wordLength.isEmpty());
+		assertEquals(wordLength.get(), "text".length());
+
+		// Option.some(null) is valid in Vavr, which may result in unexpected behavior of map()
+		Option<Integer> noWordLength = Option.of("text")
+				// next line results in Option.some(null) which is valid in Vavr, may result in NPE down the road
+				// .map(s -> (String)null)
+				.flatMap(s -> Option.<String>of(null)) // this results in Option.none() which is NPE-safe
+				.map(String::length);
+
+		assertTrue(noWordLength.isEmpty());
 	}
 
-	static Integer helperMethodRight() {
-		return Option.of("hello")
-				.map(str -> (String) null) // results in Option.some(null)
-				.flatMap(str -> Option.of(str)) // str is null; Option.of(null) yields Option.none()
-				.map(str -> str.length()) // no exception, valid call for Option.none()
-				.getOrElse(-1);
+	@Test
+	void getOrElse() {
+		assertEquals(Option.none().getOrElse(3), Integer.valueOf(3));
+		assertEquals(Option.none().getOrElse(() -> 3), Integer.valueOf(3));
 	}
 }
