09849c33bdf36f60a08721c9c6d3e7ff3a4d942a
Sergei <sleepytomcat@gmail.com>
2021-05-01 23:05:39 -0700

    Value to Java collections

--- src/test/java/tests/TestValue.java
+++ src/test/java/tests/TestValue.java
@@ -1,9 +1,11 @@
 package tests;
 
+import io.vavr.Tuple2;
 import io.vavr.Value;
 import io.vavr.collection.List;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -58,9 +60,45 @@ public class TestValue {
 
 	@Test
 	void correspondsAndEquals() {
-		assertTrue(List.of(1,2,3).corresponds(Arrays.asList(1,2,3), (x, y) -> x == y));
+		Value<Integer> someValue = List.of(1,2,3);
+		assertTrue(someValue.corresponds(Arrays.asList(1,2,3), (x, y) -> x == y));
+		assertFalse(someValue.equals(Arrays.asList(1,2,3)));
+		assertTrue(someValue.eq(Arrays.asList(1,2,3)));
+	}
 
-		assertFalse(List.of(1,2,3).equals(Arrays.asList(1,2,3)));
-		assertTrue(List.of(1,2,3).eq(Arrays.asList(1,2,3)));
+	@Test
+	void toJavaCollections() {
+		Value<Integer> someValue = List.of(3,4,5);
+
+		Object[] javaArray = someValue.toJavaArray();
+		assertEquals(3, javaArray.length);
+		assertEquals(3, javaArray[0]);
+		assertEquals(4, javaArray[1]);
+		assertEquals(5, javaArray[2]);
+
+		java.util.Collection<Integer> javaCollection = someValue.toJavaCollection(capacity -> new ArrayList<>(capacity));
+		assertEquals(3, javaCollection.size());
+		assertTrue(javaCollection.containsAll(Arrays.asList(5,4,3)));
+
+		java.util.List<Integer> javaList = someValue.toJavaList();
+		assertEquals(3, javaList.size());
+		assertTrue(javaList.containsAll(Arrays.asList(5,4,3)));
+
+		java.util.Map<Integer, String> javaMap = someValue.toJavaMap((Integer element) -> new Tuple2<>(element, String.valueOf(element)));
+		assertEquals(3, javaMap.size());
+		assertEquals("3", javaMap.get(3));
+		assertEquals("4", javaMap.get(4));
+		assertEquals("5", javaMap.get(5));
+
+		java.util.Optional<Integer> javaOptional = someValue.toJavaOptional();
+		assertFalse(javaOptional.isEmpty());
+		assertEquals(3, javaOptional.get()); // takes first element of the Value
+
+		java.util.Set<Integer> javaSet = someValue.toJavaSet();
+		assertEquals(3, javaSet.size());
+		assertTrue(javaSet.containsAll(Arrays.asList(5,4,3)));
+
+		java.util.stream.Stream<Integer> javaStream = someValue.toJavaStream();
+		assertEquals(3, javaStream.count());
 	}
 }
